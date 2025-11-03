f8fb8c3f4214110343212d17fbe309468e45dcd2
türkay ürkmez <turkay.urkmez@outlook.com>
2025-11-03 16:23:55 +0300

    feat: implement menu component and enhance Todo app functionality with task filtering

--- todo-app-demo/src/app/app.component.ts
+++ todo-app-demo/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { TodoItem } from './models/todoItem.model'
+import { TodoItem } from './models/todoItem.model';
+import { todoList } from './models/todoItem.mock';
 
 @Component({
   selector: 'app-root',
@@ -17,11 +18,7 @@ export class AppComponent {
 
   myTodoItem: TodoItem = new TodoItem('Çiçekleri sula',false);
 
-  todoList: TodoItem[] = [
-    new TodoItem('Angular Öğren',false),
-    new TodoItem('.net ile api uygulaması geliştir',true),
-    new TodoItem('DB oluştur',false)
-  ];
+  todoList: TodoItem[] = todoList;
 
   textData:string ='';
   
@@ -33,13 +30,31 @@ export class AppComponent {
     
   }
 
-  getCompletedItemsCount(): number{
+  getInCompletedItemsCount(): number{
 
     //todoList array'i içindeki isDone özelliği true olanları filtrele ve
     //eleman sayısını döndür.
 
-    return this.todoList.filter(t => t.isDone).length;
+    return todoList.filter(t => !t.isDone).length;
 
+  }
+
+  isAllTaskDisplayed: boolean = true;
+  customButtonText:string = 'Yapılan işleri gizle';
+
+  filter():void{
+    console.log('tıklandı...');
+    this.isAllTaskDisplayed = !this.isAllTaskDisplayed;
+    //tüm yapılacaklar gösterilecekse, tüm kloleksiyonu getir, sadece yapılmamış olanlar gösterilecekse filtrele.
+   this.isAllTaskDisplayed ? this.todoList = todoList : 
+                             this.todoList =  this.todoList.filter(t => !t.isDone);
+
+   this.isAllTaskDisplayed ? this.customButtonText = 'Yapılan işleri gizle': 
+                             this.customButtonText = 'Tüm görevleri göster';
+  }
+
+  getButtonText():string{
+    return this.isAllTaskDisplayed ? 'Yapılan işleri gizle':'Tüm görevleri göster';
   }
 
 
---
+++ todo-app-demo/src/app/models/todoItem.mock.ts
@@ -0,0 +1,9 @@
+import { TodoItem } from "./todoItem.model";
+
+export const todoList : TodoItem[]=[
+    new TodoItem('Angular Öğren',false),
+    new TodoItem('.net ile api uygulaması geliştir',true),
+    new TodoItem('DB oluştur',false)
+  ];
+
+  
